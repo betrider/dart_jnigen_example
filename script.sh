5c647a7bb49292e36bd14c76d430732bc7270511
betrider <tlsfpem@gmail.com>
2023-08-25 14:10:55 +0900

    jni 관련파일 업로드

--- script.sh
+++ script.sh
@@ -1,4 +1,13 @@
+## jdk11 추천
+
+# src 폴더 생성
 1.dart run jnigen --config jnigen.yaml
+
+# build 폴더 생성
 2.dart run jni:setup -p jni -s src/example
+
+# class 파일 생성
 3.javac java/dev/dart/Example.java
-4.dart run darT_jnigen_example:sum 17 25
+
+# 실행
+4.dart run dart_jnigen_example:sum 17 25

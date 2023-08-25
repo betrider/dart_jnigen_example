fa9b9b45fad321146a191ec7b9893802d35b2f80
betrider <tlsfpem@gmail.com>
2023-08-25 14:25:35 +0900

    merge

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 ## jdk11 추천
 
-# src 폴더 생성
+# .c 파일 && .dart 파일 생성
 1.dart run jnigen --config jnigen.yaml
 
-# build 폴더 생성
+# .dylib 파일 생성
 2.dart run jni:setup -p jni -s src/example
 
-# class 파일 생성
+# .class 파일 생성
 3.javac java/dev/dart/Example.java
 
 # 실행
